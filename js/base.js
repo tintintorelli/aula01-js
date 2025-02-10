b08d473636de6a78170e72ac50e05b028833874c
Daniel <git0123445@gmail.com>
2025-02-10 10:41:22 -0300

    01- Criando o arquivo index.html e o arquivo incial de js

---
+++ js/base.js
@@ -0,0 +1,25 @@
+// Exibir o console
+// console so existe no navegador 
+console.log('Olá, sou o console');
+console.log('Eu exibo conteúdo');
+
+// variáveis
+let rmAluno = 333;
+let nomeAluno = 'Rivaldo';
+let turmaAluno = '3TI';
+const ESCOLA_ALUNO = 'Lourdão';
+
+console.log(rmAluno, nomeAluno);
+console.log(turmaAluno, ESCOLA_ALUNO);
+
+// concatenação - péssima prática, mas muito comum
+console.log('Nome do aluno: ' + nomeAluno);
+
+// Template string 
+console.log(`Nome do aluno: ${nomeAluno} Rm do aluno: ${rmAluno}`);
+
+turmaAluno = '3A'
+console.log(`Turma do aluno: ${turmaAluno}`);
+
+turmaAluno = '3AC'
+console.log(`Turma do aluno: ${turmaAluno}`);
